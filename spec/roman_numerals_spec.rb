91730cb8bab1adb8441cd99df5c5e580c4b648e7
Nathan Quayle <nathan.quayle@gmail.com>
2018-08-30 09:58:55 +0000

    Added r_table to handle roman numeral values

--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -1,6 +1,17 @@
 require "roman_numerals"
+
 describe "#translate" do
-  it "returns 'hi' when passed everything" do
-    expect(translate("a")).to eq "hi"
+  
+  it "returns 'I' when passed 1" do
+    expect(translate(1)).to eq "I"
   end
+  
+  it "returns 'II' when passed 2" do
+    expect(translate(2)).to eq "II"
+  end
+  
+  it "returns 'IV' when passed 4" do
+    expect(translate(4)).to eq "IV"
+  end
+  
 end
